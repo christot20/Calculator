96470b08b5ac04fcc70f85ea52d436761320fb0b
christot20 <thomaschristo1234@yahoo.com>
2021-11-22 13:13:10 -0500

    removed console.logs)

--- script.js
+++ script.js
@@ -21,14 +21,12 @@ const keyd = document.addEventListener('keydown', function(event) {
         operate();
     }
     else if((event.key === "/") || (event.key === "+") || (event.key === "-") || (event.key === "*")) {
-        console.log("gi")
         j = event.key
         ops();
     }
     else if((event.key === "1") || (event.key === "2") || (event.key === "3") || (event.key === "4") || 
     (event.key === "5") || (event.key === "6") || (event.key === "7") || (event.key === "8") || 
     (event.key === "9")) {
-        console.log("gi")
         n = event.key
         nums();
     }
@@ -36,7 +34,6 @@ const keyd = document.addEventListener('keydown', function(event) {
 
 displayedAnswer.textContent = ""; 
 function displayedNumber(){
-    console.log("sus")
     if (queue.length === 0){
         displayedAnswer.textContent = ""
     }
@@ -55,47 +52,37 @@ function displayedNumber(){
 }
 
 function clearAll(e){
-    console.log(e)
     operators = ""
     displayValue = ""
     displayValue2 = ""
     queue = []
-    console.log(queue)
     return displayedNumber()
 }
 function clearOne(e){       
     if(displayValue2.length !== 15){
-        console.log("yo")
         decimal.removeEventListener("click", nums);
         decimal.addEventListener("click", nums);
     }
     if(displayValue.length !== 15){
-        console.log("yo")
         decimal.removeEventListener("click", nums);
         decimal.addEventListener("click", nums);
     }
     if(queue.length === 1){
         if (displayValue.length >= 15){
             numBttn.forEach(bttn => bttn.addEventListener("click", nums)); 
-            console.log("hi")
         }
-        console.log(e)
         displayValue.toString()
         displayValue = displayValue.slice(0, displayValue.length - 1);
         queue.splice(0,1,displayValue)
-        console.log(queue)
         return displayedNumber()
     }
     else if (displayValue2 !== ""){
         if (displayValue2.length >= 15){
             numBttn.forEach(bttn => bttn.addEventListener("click", nums)); 
-            console.log("hi")
         }
-        console.log(e)
         displayValue2.toString()
         displayValue2 = displayValue2.slice(0, displayValue2.length - 1);
         queue.splice(1,1,displayValue2)
-        console.log(queue)
         return displayedNumber()
     }
 }
@@ -132,16 +119,13 @@ function divide(){
 }
 function operate(){
     if (operators === "+"){
-        console.log(queue)
         if (queue.length === 1 || (queue.length === 2 && queue[1] === "")){
             newdisplayValue = queue[0]
             return displayedNumber
         }
         else{
-            console.log(addition())
             addition()
             queue.splice(-1,1)
-            console.log(queue)
             operators = "new"
             return displayedNumber()
         }
@@ -152,10 +136,8 @@ function operate(){
             return displayedNumber
         }
         else{
-            console.log(subtraction())
             subtraction()
             queue.splice(-1,1)
-            console.log(queue)
             operators = "new"
             return displayedNumber()
         }
@@ -166,10 +148,8 @@ function operate(){
             return displayedNumber
         }
         else{
-            console.log(multiply())
             multiply()
             queue.splice(-1,1)
-            console.log(queue)
             operators = "new"
             return displayedNumber()
         }
@@ -180,10 +160,8 @@ function operate(){
             return displayedNumber
         }
         else{
-            console.log(divide())
             divide()
             queue.splice(-1,1)
-            console.log(queue)
             operators = "new"
             return displayedNumber()
         }
@@ -193,9 +171,7 @@ function operate(){
 operators = ""
 function ops(e){
     decimal.addEventListener("click", nums)
-    console.log(e)
     if (typeof e !== "undefined" || typeof j === "undefined"){
-        console.log("fhhhghgh")
         typeof j === "undefined"
         y = e.target.textContent
     }
@@ -203,9 +179,7 @@ function ops(e){
         typeof e === "undefined"                          
         y = j                                             
     }
-    console.log(y)
     if (queue.length === 1){
-        console.log("shoosh")
         displayValue = Number(queue[0])
         queue.shift()
         queue.push(displayValue)
@@ -259,11 +233,9 @@ function ops(e){
     else if (y === "="){
         if (displayValue2 === "" || queue.length < 2){
             if (displayValue === ""){
-                console.log(0)
                 return 0
             }
             else{
-                console.log(parseFloat(queue[0]))
                 return parseFloat(queue[0])
             }
         }
@@ -292,15 +264,12 @@ document.addEventListener('keydown', function(event) {
 
 function nums(e){
     if (operators === "new" && queue.length === 1){
-        console.log(queue)
         queue = []
-        console.log(queue)
         displayValue = ""
         displayValue2 = ""
         operators = ""
     }
     if (typeof e !== "undefined" || typeof n === "undefined"){
-        console.log("fhhhghgh")
         typeof n === "undefined"
         x = e.target.textContent
     }
@@ -309,8 +278,6 @@ function nums(e){
         x = n                                           
     }
     decimal.addEventListener("click", nums)
-    console.log(e);
-    console.log(x)
     if (operators !== ""){
         if (displayValue2.includes(".")){
             if (x === "."){
@@ -324,16 +291,12 @@ function nums(e){
                 displayValue2 = displayValue2.substring(0, displayValue2.length - 1);
             }
         }
-        console.log("h")
         displayValue2 += x
-        console.log(displayValue2)
         if (displayValue2.includes("..")){
-            console.log("swoos")
             displayValue2 = displayValue2.substring(0, displayValue2.length - 1);
         }
         else if (displayValue2.includes("00")){
             if((displayValue2. indexOf(".") === -1) && (displayValue2. indexOf("0") === 0)){
-                console.log("swoos")
                 displayValue2 = displayValue2.substring(0, displayValue2.length - 1);
             }
         }
@@ -343,12 +306,10 @@ function nums(e){
         || (displayValue2.indexOf("8") === 1) || (displayValue2.indexOf("9") === 1))){
             displayValue2 = displayValue2.substring(1);
         }
-        console.log(displayValue2)
         if (queue.length === 2){
             queue.splice(-1,1);
         }
         queue.push(displayValue2)
-        console.log(queue)
         displayedNumber()
     }
     else{
@@ -366,12 +327,10 @@ function nums(e){
         }
         displayValue += x
         if (displayValue.includes("..")){
-            console.log("swoos")
             displayValue = displayValue.substring(0, displayValue.length - 1);
         }
         else if (displayValue.includes("00")){
             if((displayValue. indexOf(".") === -1) && (displayValue. indexOf("0") === 0)){
-                console.log("swoos")
                 displayValue = displayValue.substring(0, displayValue.length - 1);
             }
         }
@@ -381,12 +340,10 @@ function nums(e){
         || (displayValue.indexOf("8") === 1) || (displayValue.indexOf("9") === 1))){
             displayValue = displayValue.substring(1);
         }
-        console.log(displayValue)
         queue.push(displayValue)
         if (queue.length > 1){
             queue.shift();
         }
-        console.log(queue)
         displayedNumber()
     }
 }
